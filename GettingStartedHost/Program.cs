e7a4d3d5de4d366492de2fd289d47d25a0590d34
yonathanseleshi <yonathan.seleshi@gmail.com>
2018-08-04 13:33:17 -0700

    Completed Service creation, updated app.config, service running

---
+++ GettingStartedHost/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+using GettingStartedLib;
+
+namespace GettingStartedHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Step 1 Create a URI to serve as the base address.  
+            Uri baseAddress = new Uri("http://localhost:8000/GettingStarted/");
+
+            // Step 2 Create a ServiceHost instance  
+            ServiceHost selfHost = new ServiceHost(typeof(CalculatorService), baseAddress);
+
+            try
+            {
+                // Step 3 Add a service endpoint.  
+                
+
+                // Step 4 Enable metadata exchange.  
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                smb.MetadataExporter.PolicyVersion = PolicyVersion.Policy15;
+                selfHost.Description.Behaviors.Add(smb);
+
+                selfHost.AddServiceEndpoint(
+                    ServiceMetadataBehavior.MexContractName,
+                    MetadataExchangeBindings.CreateMexHttpBinding(),
+                    "mex"
+                );
+
+                selfHost.AddServiceEndpoint(typeof(ICalculator), new WSHttpBinding(), "CalculatorService");
+
+                // Step 5 Start the service.  
+                selfHost.Open();
+                Console.WriteLine("The service is ready.");
+                Console.WriteLine("Press <ENTER> to terminate service.");
+                Console.WriteLine();
+                Console.ReadLine();
+
+                // Close the ServiceHostBase to shutdown the service.  
+                selfHost.Close();
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine("An exception occurred: {0}", ce.Message);
+                selfHost.Abort();
+            }
+        }
+    }
+}
